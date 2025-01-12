b900c418a70f03d7de9a97fca8af8125cff80b6f
Wojciech-Krol <wojtekkrool@gmail.com>
2025-01-12 16:57:21 +0100

    changes

--- app/src/main/java/lab/android/chartersapp/charters/data/repositories/BoatRepository.kt
+++ app/src/main/java/lab/android/chartersapp/charters/data/repositories/BoatRepository.kt
@@ -1,5 +1,7 @@
 package lab.android.chartersapp.charters.data.repositories
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import lab.android.chartersapp.charters.data.BoatsApiService
 import lab.android.chartersapp.charters.data.dataclasses.Boat
 import javax.inject.Inject
@@ -15,23 +17,11 @@ class BoatRepository @Inject constructor(private val apiService: BoatsApiService
         }
 
         // If the response is not successful, handle the error
-        throw Exception("Failed to fetch boats")
+        throw Exception("Failed to fetch, response code: ${response.code()}, message: ${response.message()}")
     }
 
-    suspend fun fetchBoatByName(name: String): Boat? {
-        val response = apiService.getBoats() // Assuming you have the method to fetch all boats
-        if (response.isSuccessful) {
-            response.body()?.let {
-                // Find the boat by its name
-                return it.boats.find { boat -> boat.name == name }
-            }
-        }
-        // If the response is not successful, handle the error
-        throw Exception("Failed to fetch boat by name")
-    }
-
-    suspend fun fetchBoatsByPort(port: String): List<Boat> {
-        val response = apiService.getBoatsByPort(port)
+    suspend fun fetchBoatsByPort(portName: String): List<Boat> {
+        val response = apiService.getBoatsByPort(portName)
         if (response.isSuccessful) {
             response.body()?.let {
                 // Return the list of boats
@@ -40,7 +30,58 @@ class BoatRepository @Inject constructor(private val apiService: BoatsApiService
         }
 
         // If the response is not successful, handle the error
-        throw Exception("Failed to fetch boats by port")
+        throw Exception("Failed to fetch boats by port, response code: ${response.code()}, message: ${response.message()}")
     }
 
+    suspend fun fetchBoatsByCompany(companyName: String): List<Boat> {
+        val response = apiService.getBoatsByCompany(companyName)
+        if (response.isSuccessful) {
+            response.body()?.let {
+                // Return the list of boats
+                return it.boats
+            }
+        }
+
+        // If the response is not successful, handle the error
+        throw Exception("Failed to fetch boats by company, response code: ${response.code()}, message: ${response.message()}")
+    }
+
+    suspend fun fetchBoatDetails(boatName: String): Boat {
+        val response = apiService.getBoatDetails(boatName)
+        if (response.isSuccessful) {
+            response.body()?.let {
+                // Return the boat details
+                return it
+            }
+        }
+
+        // If the response is not successful, handle the error
+        throw Exception("Failed to fetch boat details, response code: ${response.code()}, message: ${response.message()}")
+    }
+
+    suspend fun fetchBoatPhotos(boatName: String): List<String> {
+        val response = apiService.getBoatPhotos(boatName)
+        if (response.isSuccessful) {
+            response.body()?.let {
+                // Return the list of photo URLs
+                return it.map { photo -> photo.url }
+            }
+        }
+
+        // If the response is not successful, handle the error
+        throw Exception("Failed to fetch boat photos, response code: ${response.code()}, message: ${response.message()}")
+    }
+
+    suspend fun fetchPhoto(imgName: String): Bitmap? {
+        val response = apiService.getPhoto(imgName)
+        if (response.isSuccessful) {
+            response.body()?.let { responseBody ->
+                val inputStream = responseBody.byteStream()
+                return BitmapFactory.decodeStream(inputStream)
+            }
+        }
+
+        // If the response is not successful, handle the error
+        throw Exception("Failed to fetch photo, response code: ${response.code()}, message: ${response.message()}")
+    }
 }
