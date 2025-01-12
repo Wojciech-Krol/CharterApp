ad1422c045076b2aaac38be50ef2db3d6e8d1bac
Pakos <pakostasip@gmail.com>
2025-01-12 14:04:11 +0100

    Added AuthApi, ChartersApi, PortsApi and extended Boats Api

--- app/src/main/java/lab/android/chartersapp/charters/data/ApiService.kt
+++ app/src/main/java/lab/android/chartersapp/charters/data/ApiService.kt
@@ -1,8 +1,58 @@
 package lab.android.chartersapp.charters.data
 
+import lab.android.chartersapp.charters.data.dataclasses.Boat
+import lab.android.chartersapp.charters.data.dataclasses.BoatPhoto
+import lab.android.chartersapp.charters.data.dataclasses.BoatsResponse
+import lab.android.chartersapp.charters.data.dataclasses.ChartersResponse
+import lab.android.chartersapp.charters.data.dataclasses.Port
+import lab.android.chartersapp.charters.data.dataclasses.PortsResponse
 import retrofit2.http.GET
 import retrofit2.Response
-interface OfferApiService {
+import retrofit2.http.POST
+
+interface AuthApiService {
+    @POST("login/")
+    suspend fun login(): Response<Boolean>
+
+    @POST("logout/")
+    suspend fun logout(): Response<Boolean>
+
+    @POST("register/")
+    suspend fun register(): Response<Boolean>
+}
+
+interface BoatsApiService {
     @GET("boats/")
-    suspend fun getBoats(): Response<BoatResponse>
+    suspend fun getBoats(): Response<BoatsResponse>
+
+    @GET("boats/byPort/")
+    suspend fun getBoatsByPort(): Response<BoatsResponse>
+
+    @GET("boats/byCompany/")
+    suspend fun getBoatsByCompany(): Response<BoatsResponse>
+
+    @GET("boats/details/")
+    suspend fun getBoatDetails(): Response<Boat>
+
+    @GET("boatPhotos/")
+    suspend fun getBoatPhotos(): Response<List<BoatPhoto>>
+}
+
+interface ChartersApiService {
+    @GET("charters/ByBoat/")
+    suspend fun getChartersByBoat(): Response<ChartersResponse>
+
+    @GET("charters/ByUser/")
+    suspend fun getChartersByUser(): Response<ChartersResponse>
+
+    @POST("charters/add/")
+    suspend fun addCharter(): Response<Boolean>
+}
+
+interface PortsApiService {
+    @GET("ports/")
+    suspend fun getPorts(): Response<PortsResponse>
+
+    @GET("ports/details/")
+    suspend fun getPortDetails(): Response<Port>
 }
